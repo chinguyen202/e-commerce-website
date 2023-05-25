ee21c583d9701f17995413bd20b92639521bcc88
Chi Nguyen <chi.nguyen@live.com>
2023-05-26 03:44:43 +0700

    add customer list

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import {
+  Table,
+  TableContainer,
+  TableCell,
+  TableHead,
+  TableRow,
+  TablePagination,
+  TableBody,
+} from '@mui/material';
+import useAppSelector from '../hooks/useAppSelector';
+import { useAppDispatch } from '../hooks/useAppDispatch';
+import { fetchAllUsers } from '../store/store';
 
 const CustomerList = () => {
-  return <div>CustomerList</div>;
+  const dispatch = useAppDispatch();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const users = useAppSelector((state) => state.user.users);
+  useEffect(() => {
+    if (users.length === 0) {
+      dispatch(fetchAllUsers());
+    }
+  }, [users, dispatch]);
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const customers = users.filter((customer) => customer.role === 'customer');
+  const displayedUsers = customers.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
+  return (
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Role</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {displayedUsers.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.role}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        component="div"
+        rowsPerPageOptions={[5, 10, 25]}
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
 };
 
 export default CustomerList;
